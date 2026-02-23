af05dc79bcb50111f406c3955a6c6611b0c35046
Yolanda Piao <ypiao@lcs.on.ca>
2026-02-23 14:29:23 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Yolanda Piao on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
